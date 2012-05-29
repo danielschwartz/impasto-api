06dcd660b9988c46b0ca399e4c3901bf3a67a0f1
Daniel Schwartz <dschwartz88@gmail.com>
2012-05-28 20:02:25 -0400

    trying something

--- app.js
+++ app.js
@@ -52,9 +52,10 @@ switch(NodeEnv){
 		break;
 }
 
+console.log(GLOBAL.db);
+
 GLOBAL.db = new Sequelize(dbOptions.name, dbOptions.user, dbOptions.pass, {
 	host: dbOptions.host,
-	port: dbOptions.port,
 	dialect: dbOptions.dialect,
 	define: {
 		instanceMethods: {
