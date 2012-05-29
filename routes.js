7fd0ee9e3435cc60370d8edf1d67b0c49d2c9632
Daniel Schwartz <dschwartz88@gmail.com>
2012-05-28 22:04:44 -0400

    adding temp homepahe

--- routes.js
+++ routes.js
@@ -3,6 +3,12 @@ var API = require(__root + '/controllers/API');
 
 
 module.exports = function(app){
+	app.get('/', function(req, res){
+		res.render('index', {
+			title: 'Impasto API Homepage'
+		});
+	});
+
 	app.get('/api/:service/:method', API.get);
 
 	app.get('/dbseed', function(req, res){
