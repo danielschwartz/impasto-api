bc0302ab40cb998b910ce9c767822208cec84e16
Daniel Schwartz <dschwartz88@gmail.com>
2012-06-09 16:07:38 -0400

    fixing cannot set headers error

--- controllers/AdminPieceController.js
+++ controllers/AdminPieceController.js
@@ -1,29 +1,29 @@
 module.exports.list = function (req, res){
     if(!req.isAuthenticated()){
         res.redirect('/admin/login');
-    }
+    } else {
+        var pieceService = ServiceLoader.get('PieceService');
 
-
-    var pieceService = ServiceLoader.get('PieceService');
-
-    pieceService.getAllPieces(req, res, function(pieces){
-        res.render('admin/pieces/list', {
-            pieces: pieces
+        pieceService.getAllPieces(req, res, function(pieces){
+            res.render('admin/pieces/list', {
+                pieces: pieces
+            });
         });
-    });
+    }
 }
 
 module.exports.show = function(req, res){
     if(!req.isAuthenticated()){
         res.redirect('/admin/login');
-    }
-    var pieceService = ServiceLoader.get('PieceService');
-    req.query.id = req.params.id;
+    } else {
+        var pieceService = ServiceLoader.get('PieceService');
+        req.query.id = req.params.id;
 
-    pieceService.getPieceById(req, res, function(piece){
+        pieceService.getPieceById(req, res, function(piece){
 
-        res.render('admin/pieces/show', {
-            piece: piece
+            res.render('admin/pieces/show', {
+                piece: piece
+            });
         });
-    });
+    }
 }
